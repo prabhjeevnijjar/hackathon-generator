f35302a7c7d24927cc59ac902ff5a47a3c449e38
Prabhjeev <prabhjeevnijjar@gmail.com>
2024-09-02 21:45:42 +0530

    faqs section

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 import Banner from './components/HompComp/Banner';
+import Faqs from './components/HompComp/Faqs';
 import Stats from './components/HompComp/Stats';
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
     <>
       <Banner />
       <Stats />
+      <Faqs />
     </>
   );
 }
