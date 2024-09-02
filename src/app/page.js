db04e771721fd1f119dcb7a0848b5bb53ef6d472
Prabhjeev <prabhjeevnijjar@gmail.com>
2024-09-02 19:00:43 +0530

    stats section

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import Banner from './components/HompComp/Banner';
+import Stats from './components/HompComp/Stats';
 
 export default function Home() {
   return (
     <>
       <Banner />
+      <Stats />
     </>
   );
 }
