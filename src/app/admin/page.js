80f00588a1c139de7a5ea4a3e3be35b445d476e5
Prabhjeev <prabhjeevnijjar@gmail.com>
2024-09-04 19:14:04 +0530

    admin form

---
+++ src/app/admin/page.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Form from '../components/AdminComp/Form';
+
+const ChallengePage = () => {
+  return (
+    <div className="">
+      <div className="bg-[#F8F9FD]  font-bold text-[1.562rem] leading-[1.812rem]">
+        <div className="px-[2rem] md:px-[6rem] 2xl:px-[25rem] py-10 ">Challenge Details</div>
+      </div>
+      <Form />
+    </div>
+  );
+};
+
+export default ChallengePage;
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,10 +1,15 @@
-import { Poppins } from 'next/font/google';
+import { Poppins, Inter } from 'next/font/google';
 import './globals.css';
 import Header from './components/CommonComp/Header';
 
 const poppins = Poppins({
   subsets: ['latin'],
   weight: ['400', '500', '600', '700', '800', '900'],
+});
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter',
+  display: 'swap',
 });
 export const metadata = {
   title: 'Hackathon Generator',
@@ -14,7 +19,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${poppins.className}`}>
+      <body className={`${poppins.className} ${inter.variable} `}>
         <Header />
         {children}
       </body>
